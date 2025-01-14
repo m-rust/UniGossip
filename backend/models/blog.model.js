9ade67ffb297a300d5a2cf619279fe51e63f7142
garvit jawa <garvitjawa@garvits-MacBook-Air.local>
2025-01-15 00:50:38 +0530

    added user schema

--- backend/models/blog.model.js
+++ backend/models/blog.model.js
@@ -1,29 +1,28 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema(
-	{
-		image: {
-			type: String,
-			required: true,
-		},
-		gossip: {
-			type: String,
-			required: true,
-		},
-		time: {
-			type: String,
-			required: true,
-		},
-        likes: {
-            type: Number,
-            required:true
-        },
-		uname: {
-			type: String,
-            required:true
-		},
-	}
-);
+const blogSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+  gossip: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  uname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
 const Blog = mongoose.model("Blog", blogSchema);
 
---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  uname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
